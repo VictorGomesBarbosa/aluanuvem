e22f2e832913215525e982276ded41966fb63fce
ALUNO C30 <alunoc30@unifor.local>
2025-04-24 22:21:56 -0300

    funcionando

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ app.get('/', (req, res) => {
     res.send('Olá, servidor Node.js com Express funcionando!');
 });
 app.get('/info', (req, res) => {
-    res.json({ linguagem: 'Node.js', biblioteca: 'Express', nome: "victor" });
+    res.json({ linguagem: 'Node.js', biblioteca: 'Express', nome: "victor",hora: moment().format('DD/MM HH:mm:ss') });
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+const moment = require('moment');
+console.log("Data atual:", moment().format('DD/MM/YYYY HH:mm:ss'));
